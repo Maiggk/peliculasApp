2c0ea6a103db468115c06fa708906ef15f431968
Maiggk <miguelangelalvarez3mail.com>
2020-07-22 11:48:57 -0500

    Ajuste al pipe y uso del mismo en vistas

--- src/app/pipes/pelicula-img.pipe.ts
+++ src/app/pipes/pelicula-img.pipe.ts
@@ -6,17 +6,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PeliculaImgPipe implements PipeTransform {
 
 
-  transform(pelicula:any = {}): any {
+  transform(pelicula:any = {}, poster:boolean = false): any {
 
 		let url = "https://image.tmdb.org/t/p/w300/";
 
-	  if(pelicula['poster_path']){
-      return url + pelicula['poster_path'];
-	  }else{
-		  if(pelicula['backdrop_path']){
-			return url + pelicula['backdrop_path'];
+    if(poster){
+      if(pelicula['poster_path']){
+			return url + pelicula['poster_path'];
 		  }else{
-			return "assets/img/no-image.jpg";
+			return "assets/img/no-image.png";
+		  }
+    }
+
+	  if(pelicula['backdrop_path']){
+      return url + pelicula['backdrop_path'];
+	  }else{
+		  if(pelicula['poster_path']){
+			return url + pelicula['poster_path'];
+		  }else{
+			return "assets/img/no-image.png";
 		  }
 	  }
 
